a71c8fe6f8e45dad1e18c30ec8a924a601e163c2
Jaume Tarradas Llort <jaume@bic.cat>
2018-09-12 18:33:36 +0200

    added --no-sandbox

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch({
-    // headless: false,
+    args: ['--no-sandbox']
   });
   const page = await browser.newPage();
 
