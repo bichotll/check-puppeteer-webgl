959fbdbbc1e4ea3628b9f29ba2a34ad93904abf7
Jaume Tarradas Llort <jaume@bic.cat>
2018-09-12 18:25:51 +0200

    👋

---
+++ index.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    // headless: false,
+  });
+  const page = await browser.newPage();
+
+  // Test for webgl support
+  // e.g. https://developer.mozilla.org/en-US/docs/Learn/WebGL/By_example/Detect_WebGL
+  const webgl = await page.evaluate(() => {
+    const canvas = document.createElement('canvas');
+    const gl = canvas.getContext('webgl');
+    const expGl = canvas.getContext('experimental-webgl');
+
+    return {
+      gl: gl && gl instanceof WebGLRenderingContext,
+      expGl: expGl && expGl instanceof WebGLRenderingContext,
+    };
+  });
+
+  console.log('WebGL Support:', webgl);
+
+  await browser.close();
+})();
